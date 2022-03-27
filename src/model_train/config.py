fe2a1658e0fd7579a61e9da517e82eae709cf694
terry <jkchen14@gmail.com>
2022-03-27 14:52:44 +0800

    new S

--- src/model_train/config.py
+++ src/model_train/config.py
@@ -2,7 +2,7 @@ DATA_NAME = "accel_ms2_xyz"
 LABEL_NAME = "gesture"
 
 # label name (you should keep "negative" in the end of the list)
-labels = ["ring", "slope", "negative"]
+labels = ["ring", "slope", "S", "negative"]
 
 # data split configuration
 # note that train_ratio + valid_ratio + test_ratio = 1
